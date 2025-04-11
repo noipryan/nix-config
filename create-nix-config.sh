f076fbfccc888bad59fb663b66c80b71d13b6c93
Ryan <ryan@starflyer.dev>
2025-04-11 15:15:35 -0400

    Updated install script

--- create-nix-config.sh
+++ create-nix-config.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
 
+# Stop script on error
 set -e
 
-NIX_CONFIG="/mnt/etc/nixos/configuration.nix"
+# Generate separator
+SEP=$(printf "%0.s#" {1..80})
+
+echo $SEP
 read -p "Enter hostname: " NIX_HOSTNAME
 #read -p "Enter full name: " NIX_FULLNAME
-#read -p "Enter username: " NIX_USER
+read -p "Enter username: " NIX_USER
 #read -s -p "Enter Password: " NIX_PASSWORD
+echo $SEP
+
+NIX_CONFIG="/mnt/etc/nixos/configuration.nix"
+NIX_HARDWARE="/mnt/etc/nixos/hardware-configuration.nix"
+NIX_CONFIG_DIR="/root/nix-config/${NIX_HOSTNAME}"
 
 # Generate a new config
 nixos-generate-config --force --root /mnt
 
 #echo "Updating bootloader config"
 #sed -i '/boot.loader.grub.device/a\  boot.loader.grub.device = "/dev/vda";'  $NIX_CONFIG
-
+echo $SEP
 echo "Setting Hostname to $NIX_HOSTNAME"
 sed -i "s/\# networking.hostName = \"nixos\"; /networking.hostName = \"$NIX_HOSTNAME\"; /" $NIX_CONFIG
 
@@ -38,6 +47,24 @@ sed -i '/boot loader/i\  # Enable experimental features \n  nix.settings.experim
 echo "Enabling SSH"
 sed -i "s/\# services.openssh.enable/services.openssh.enable/" $NIX_CONFIG
 
+echo "Cleaning up default config"
+sed -i '/Pick only one of/,/Enable the OpenSSH daemon/d' $NIX_CONFIG
+
+sleep 2
+echo $SEP
+
+if [ -d "${NIX_CONFIG_DIR}" ]; then
+  echo "Current configuration exists for ${NIX_HOSTNAME}."
+  echo "Copying hardware-configuration.nix to ${NIX_CONFIG_DIR}"
+  cp ${NIX_HARDWARE} $NIX_CONFIG_DIR}/hardware-configuration.nix
+else
+  echo "No configuration exists for ${NIX_HOSTNAME}. Creating folder structure in nix-config repo"
+  mkdir ${NIX_CONFIG_DIR}/hosts/${NIX_HOSTNAME}
+  echo "Copying configuration.nix & hardware-configuration.nix to ${NIX_CONFIG_DIR}"
+  cp ${NIX_CONFIG} ${NIX_CONFIG_DIR}/configuration.nix
+  cp ${NIX_HARDWARE} $NIX_CONFIG_DIR}/hardware-configuration.nix
+fi
+echo $SEP
 #echo "Going into /mnt and running nixos-install --no-root-password"
 #cd /mnt
 #nixos-install --no-root-password
@@ -50,6 +77,21 @@ sed -i "s/\# services.openssh.enable/services.openssh.enable/" $NIX_CONFIG
 #
 #echo "Removing initial password from config file"
 #sed -i "/initialPassword/d" $NIX_CONFIG
+echo $SEP
+echo "Exporting vars for install script"
 
-echo "Nixos config generated."
-#systemctl reboot
+export NIX_HOSTNAME=${NIX_HOSTNAME}
+export NIX_USER=${NIX_USER}
+
+echo $SEP
+
+echo "The hostname is: ${NIX_HOSTNAME}"
+echo "The user is: ${NIX_USER}"
+
+echo $SEP
+
+echo "Initial configuration completed."
+echo "Please update the ${NIX_CONFIG_DIR}/configuration.nix with any changes and add the system to flake.nix"
+echo "Run the install-nix.sh script in the nix-config folder to install the os"
+
+echo $SEP
