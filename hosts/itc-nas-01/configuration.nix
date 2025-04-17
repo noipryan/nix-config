d3ef0d2b8c5909f0da5210cbd1632c1c5c73f9e0
Ryan <ryan@starflyer.dev>
2025-04-17 16:33:23 -0400

    Added local-packages

--- hosts/itc-nas-01/configuration.nix
+++ hosts/itc-nas-01/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./local-packages.nix
       ../../users/swainrl.nix
       ../../modules/localization.nix
       ../../modules/env.nix
