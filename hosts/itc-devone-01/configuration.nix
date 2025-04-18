2b258d7c8f7442dbe3a76182e139c3b6ef75b26b
Ryan <ryan@starflyer.dev>
2025-04-18 08:45:17 -0400

    Updated itc-devon-01

--- hosts/itc-devone-01/configuration.nix
+++ hosts/itc-devone-01/configuration.nix
@@ -8,11 +8,19 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ../../users/swainrl.nix
+      ../../modules/env.nix
+      ../../modules/fonts.nix
+      ../../modules/localization.nix
+      ../../modules/sddm.nix
+      ../../modules/sway.nix
+      ../../modules/tailscale.nix
+      ../../modules/zsh.nix
     ];
 
-  # Enable experimental features 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ]; 
- 
+  # Enable experimental features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -22,85 +30,8 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
   networking.networkmanager.wifi.backend = "iwd";
-  #networking.wireless.iwd.enable = true;
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-
-  
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Enable ZSH 
-  programs.zsh.enable = true; 
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.swainrl = { 
-     isNormalUser = true; 
-     description = "Ryan Swain"; 
-     extraGroups = [ "networkmanager" "wheel" "audio" "input" "libvirtd" "video" ]; 
-     packages = with pkgs; [ 
-     #  thunderbird 
-     ]; 
-     shell = pkgs.zsh; 
-   };
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     tree
-  #   ];
-  # };
-
-  # programs.firefox.enable = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
+    # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
@@ -136,4 +67,3 @@
   system.stateVersion = "24.11"; # Did you read the comment?
 
 }
-
