54eeb39230779cc67804634523f163db7ff98539
Ryan <ryan@starflyer.dev>
2025-04-21 16:10:10 -0400

    testing

--- hosts/nixos-test/configuration.nix
+++ hosts/nixos-test/configuration.nix
@@ -2,45 +2,112 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, pkgs-unstable, ... }:
-# let
-#   unstable = import <unstable> { config = { allowUnfree = true; }; };
-# in
+{ config, lib, pkgs, ... }:
+
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ../../users/swainrl.nix
-      ../../modules/env.nix
-      ../../modules/fonts.nix
-      ../../modules/kvm-guest.nix
-      ../../modules/localization.nix
-      ../../modules/packages.nix
-      ../../modules/sddm.nix
-      ../../modules/xfce.nix
-      ../../modules/zsh.nix
     ];
 
-  # Allow unfree packages
-  # nixpkgs.config.allowUnfree = true;
+  # Enable experimental features 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ]; 
+ 
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  boot.loader.grub.device = "/dev/vda";
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nixos-test"; # Define your hostname.
+  networking.hostName = "nixos-bios"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+
+  
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # hardware.pulseaudio.enable = true;
+  # OR
+  # services.pipewire = {
+  #   enable = true;
+  #   pulse.enable = true;
+  # };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Enable ZSH 
+  programs.zsh.enable = true; 
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.swainrl = { 
+     isNormalUser = true; 
+     description = "Ryan"; 
+     extraGroups = [ "networkmanager" "wheel" "audio" "input" "video" ]; 
+     packages = with pkgs; [ 
+     #  thunderbird 
+     ]; 
+     shell = pkgs.zsh; 
+   };
+  # users.users.alice = {
+  #   isNormalUser = true;
+  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  #   packages = with pkgs; [
+  #     tree
+  #   ];
+  # };
+
+  # programs.firefox.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #   wget
+  # ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-
-  # Enable nix-ld
-  programs.nix-ld.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -73,3 +140,4 @@
   system.stateVersion = "24.11"; # Did you read the comment?
 
 }
+
