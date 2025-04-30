21568b4de87a87f9cab3b419cf89d4e0211ad713
Ryan Swain <ryan@starflyer.dev>
2025-04-30 10:21:19 -0400

    Updated config

--- hosts/itc-x270-01/configuration.nix
+++ hosts/itc-x270-01/configuration.nix
@@ -2,10 +2,8 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs,... }:
-# let
-#   unstable = import <unstable> { config = { allowUnfree = true; }; };
-# in
+{ config, lib, pkgs, ... }:
+
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -24,9 +22,7 @@
       ../../modules/zsh.nix
     ];
 
-  # Allow unfree packages
-  # nixpkgs.config.allowUnfree = true;
-
+  # Enable experimental features
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Use the systemd-boot EFI boot loader.
@@ -35,106 +31,9 @@
 
   networking.hostName = "itc-x270-01"; # Define your hostname.
   # Pick only one of the below networking options.
-  #networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-  #networking.networkmanager.wifi.backend = "iwd";
-  networking.wireless.iwd.enable = true;
-
-  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.wireless.userControlled.enable = true;
-  # networking.wireless.allowAuxiliaryImperativeNetworks = true;
-  # systemd.services.wpa_supplicant.environment.OPENSSL_CONF = pkgs.writeText "openssl.cnf" ''
-  #   openssl_conf = openssl_init
-  #   [openssl_init]
-  #   ssl_conf = ssl_sect
-  #   [ssl_sect]
-  #   system_default = system_default_sect
-  #   [system_default_sect]
-  #   Options = UnsafeLegacyRenegotiation
-  #   [system_default_sect]
-  #   CipherString = Default:@SECLEVEL=0
-  # '';
-
-  # Set your time zone.
-  time.timeZone = "America/New_York";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Plymouth on boot
-  boot.plymouth.enable = true;
-  boot.initrd.systemd.enable = true;
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the Cinnamon Desktop Environment.
-  # services.xserver.displayManager.lightdm.enable = true;
-  # services.xserver.desktopManager.cinnamon.enable = true;
-
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Enable ZSH
-  programs.zsh.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.swainrl = {
-    isNormalUser = true;
-    description = "Ryan Swain";
-    extraGroups = [ "networkmanager" "wheel" "audio" "input" "video"];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
-    shell = pkgs.zsh;
-  };
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.wifi.backend = "iwd";
 
   # List services that you want to enable:
 
